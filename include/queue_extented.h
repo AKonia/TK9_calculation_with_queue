6be9360d359a088b17d35c1409baf8d78e47d663
Alexandr Konakov <konakov@my.com>
2016-11-18 01:01:49 +0300

    Прошёлся cpplint'ом

--- include/queue_extented.h
+++ include/queue_extented.h
@@ -1,32 +1,29 @@
-#ifndef __QUEUE_EXTENTED__
-#define __QUEUE_EXTENTED__
+// Copyright 2016 AuthorName. All rights reserved.
+#ifndef INCLUDE_QUEUE_EXTENTED_H_
+#define INCLUDE_QUEUE_EXTENTED_H_
 #include <queue>
-using namespace std;
-
+using std::queue;
 template <typename TypeOfElems>
-TypeOfElems * getArrayOfAverageSums(queue<TypeOfElems> q, int &size, const int w)
-{
-	if(w > q.size())
-		throw "Too big input of W";
-	size = q.size() - w+1;
-	TypeOfElems * arr = new TypeOfElems[size];
+TypeOfElems * getArrayOfAverageSums(queue<TypeOfElems> q,
+int &size, const int w) {
+  if (w > q.size())
+    throw "Too big input of W";
+  size = q.size() - w+1;
+  TypeOfElems * arr = new TypeOfElems[size];
     queue<TypeOfElems> buf = q;
-	for(int i = 0; i < size; i++)
+  for (int i = 0; i < size; i++)
         arr[i] = 0;
-
-    for(int i = 0; i< w; i++)
-    {
+    for (int i = 0; i< w; i++) {
         arr[0] += q.front();
         q.pop();
     }
-    for(int i = 1; i < size; i++)
-    {
+    for (int i = 1; i < size; i++) {
         arr[i] = arr[i-1] + q.front() - buf.front();
         q.pop();
         buf.pop();
     }
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
         arr[i] /= w;
     return arr;
 }
-#endif  // __QUEUE_EXTENTED__
+#endif  // INCLUDE_QUEUE_EXTENTED_H_
--- samples/core.cpp
+++ samples/core.cpp
@@ -1,16 +1,16 @@
+// Copyright 2016 AuthorName. All rights reserved.
 #include "queue_extented.h"
 #include <iostream>
-using namespace std;
-
-int main()
-{
+using std::cout;
+using std::queue;
+int main() {
     queue<int> q;
-	for(int i = 1; i < 10; i++)
-		q.push(i);
-	int sizeOfArr = 0; 
-	int * arr = getArrayOfAverageSums(q, sizeOfArr, 3);
-	for(int i = 0; i < sizeOfArr; i++)
-		cout << arr[i]  << " ";
-	delete [] arr;
+  for (int i = 1; i < 10; i++)
+    q.push(i);
+  int sizeOfArr = 0;
+  int * arr = getArrayOfAverageSums(q, sizeOfArr, 3);
+  for (int i = 0; i < sizeOfArr; i++)
+    cout << arr[i]  << " ";
+  delete [] arr;
     return 0;
 }
